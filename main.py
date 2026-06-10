d2e0274eed4de2f516913fd5c701d41bcfe9fd3a
TejasLamba2006 <tejas22feb@gmail.com>
2026-06-10 13:12:26 +0530

    init files

---
+++ main.py
@@ -0,0 +1,10 @@
+# pip install accelerate
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+checkpoint = "HuggingFaceTB/SmolLM2-360M"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+# for fp16 use `torch_dtype=torch.float16` instead
+model = AutoModelForCausalLM.from_pretrained(checkpoint, device_map="auto", torch_dtype=torch.bfloat16)
+inputs = tokenizer.encode("Gravity is", return_tensors="pt").to("cuda")
+outputs = model.generate(inputs)
+print(tokenizer.decode(outputs[0]))
