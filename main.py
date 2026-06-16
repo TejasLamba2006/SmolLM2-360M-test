dfc5e9ec7e58558e632460f66c048a95e3b19cc4
TejasLamba2006 <tejas22feb@gmail.com>
2026-06-16 12:01:00 +0530

    add init files

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 checkpoint = "HuggingFaceTB/SmolLM2-360M"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-# for fp16 use `torch_dtype=torch.float16` instead
 model = AutoModelForCausalLM.from_pretrained(
     checkpoint, device_map="auto", torch_dtype=torch.float32)
 inputs = tokenizer.encode("Gravity is", return_tensors="pt")
